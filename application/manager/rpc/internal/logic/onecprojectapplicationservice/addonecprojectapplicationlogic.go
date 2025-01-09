05151486b31ac5fb3fcb1651374334ce71131e52
Yan Shicheng <ikubeops@gmail.com>
2025-01-09 18:25:40 +0800

    feat(application): 添加创建命名空间和资源配额的功能
    - 在 AddOnecProjectApplicationLogic 中实现创建命名空间和资源配额的逻辑
    - 新增 createNamespace 和 createResourceQuota 函数用于创建命名空间和资源配额
    - 在 manager.proto 中添加 secretLimit 字段，用于限制应用允许创建的 Secret 数量
    - 优化了 AddOnecProjectApplicationReq 和 AddOnecProjectApplicationResp 的字段顺序- 新增 ValidateString 函数用于验证字符串是否符合规则

--- application/manager/rpc/internal/logic/onecprojectapplicationservice/addonecprojectapplicationlogic.go
+++ application/manager/rpc/internal/logic/onecprojectapplicationservice/addonecprojectapplicationlogic.go
@@ -2,6 +2,11 @@ package onecprojectapplicationservicelogic
 
 import (
 	"context"
+	"github.com/yanshicheng/kube-onec/application/manager/rpc/internal/code"
+	"github.com/yanshicheng/kube-onec/application/manager/rpc/internal/shared"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/yanshicheng/kube-onec/application/manager/rpc/internal/svc"
 	"github.com/yanshicheng/kube-onec/application/manager/rpc/pb"
@@ -25,7 +30,79 @@ func NewAddOnecProjectApplicationLogic(ctx context.Context, svcCtx *svc.ServiceC
 
 // -----------------------应用表，-----------------------
 func (l *AddOnecProjectApplicationLogic) AddOnecProjectApplication(in *pb.AddOnecProjectApplicationReq) (*pb.AddOnecProjectApplicationResp, error) {
-	// todo: add your logic here and delete this line
+	name := "testsss"
+	client, err := shared.GetK8sClient(l.ctx, l.svcCtx, "cee07e52-6516-4773-915d-0c762ed03d3e")
+	if err != nil {
+		l.Logger.Errorf("获取集群客户端异常: %v", err)
+		return nil, code.GetClusterClientErr
+	}
 
+	ns := client.GetNamespaceClient()
+	ns.CreateNamespace(&corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+	})
+	ns.SetResourceQuota(name, &corev1.ResourceQuota{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test",
+			Namespace: name,
+		},
+		Spec: corev1.ResourceQuotaSpec{
+			Hard: corev1.ResourceList{
+				corev1.ResourceCPU:                    resource.MustParse("100m"),
+				corev1.ResourceMemory:                 resource.MustParse("100Mi"),
+				corev1.ResourcePods:                   resource.MustParse("10"),
+				corev1.ResourceStorage:                resource.MustParse("10Gi"),
+				corev1.ResourceSecrets:                resource.MustParse("10"),
+				corev1.ResourceServices:               resource.MustParse("10"),
+				corev1.ResourceConfigMaps:             resource.MustParse("10"),
+				corev1.ResourcePersistentVolumeClaims: resource.MustParse("10"),
+				corev1.ResourceReplicationControllers: resource.MustParse("10"),
+				corev1.ResourceQuotas:                 resource.MustParse("10"),
+				corev1.ResourceServicesNodePorts:      resource.MustParse("10"),
+				corev1.ResourceServicesLoadBalancers:  resource.MustParse("10"),
+				corev1.ResourceLimitsCPU:              resource.MustParse("100m"),
+				corev1.ResourceLimitsMemory:           resource.MustParse("100Mi"),
+			},
+		},
+	})
 	return &pb.AddOnecProjectApplicationResp{}, nil
+}
+
+func createResourceQuota(name, namespace string) *corev1.ResourceQuota {
+	return &corev1.ResourceQuota{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: corev1.ResourceQuotaSpec{
+			Hard: corev1.ResourceList{
+				corev1.ResourceCPU:                    resource.MustParse("10"),   // 限制 10 CPU
+				corev1.ResourceMemory:                 resource.MustParse("10Gi"), // 限制 10 Gi 内存
+				corev1.ResourcePods:                   resource.MustParse("100"),  // 限制 100 个 Pod
+				corev1.ResourceServices:               resource.MustParse("10"),   // 限制 10 个 Service
+				corev1.ResourceSecrets:                resource.MustParse("10"),   // 限制 10 个 Secret
+				corev1.ResourceConfigMaps:             resource.MustParse("10"),   // 限制 10 个 ConfigMap
+				corev1.ResourceServicesNodePorts:      resource.MustParse("10"),   // 限制 10 个 NodePort
+				corev1.ResourceStorage:                resource.MustParse("10Gi"), // 限制 10 Gi 存储
+				corev1.ResourcePersistentVolumeClaims: resource.MustParse("20"),   // 限制 20 个 PVC
+			},
+		},
+	}
+}
+
+func createNamespace(name string) *corev1.Namespace {
+	return &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name, // 命名空间名称
+			Labels: map[string]string{
+				"project": "ikubeops", // 标签：project=ikubeops
+			},
+			Annotations: map[string]string{
+				"Official website address": "https://www.ikubeops.com", // 注解：官网地址
+				"作者":                       "yanshicheng",              // 注解：作者
+			},
+		},
+	}
 }
--- utils/re.go
+++ utils/re.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"regexp"
 	"strings"
 )
@@ -9,4 +10,23 @@ import (
 func RemoveQueryADN(queryStr strings.Builder) string {
 	re := regexp.MustCompile(`(?i)\bAND\b\s*$`)
 	return re.ReplaceAllString(queryStr.String(), " ")
+}
+
+// ValidateString 验证字符串是否符合规则
+func ValidateString(input string) error {
+	// 检查长度是否超过 20 位
+	if len(input) > 20 {
+		return errors.New("长度超过 20 个字符")
+	}
+
+	// 检查是否符合规则：开头为数字，包含小写字母和中横线，中横线不能结尾
+	matched, err := regexp.MatchString(`^[0-9][a-z0-9-]*[a-z0-9]$`, input)
+	if err != nil {
+		return err
+	}
+	if !matched {
+		return errors.New("字符串不符合规则：开头必须是数字，结尾不能是中横线，只能包含小写字母、数字和中横线")
+	}
+
+	return nil
 }
